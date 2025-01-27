b0282f0189e200c663ddc888dad5c8e83fd53862
Benjamin Dehli <superelg@gmail.com>
2025-01-27 11:28:54 +0100

    Add pre validations and error lists for uploaded files

--- src/scripts/fileDropzoneHelpers.js
+++ src/scripts/fileDropzoneHelpers.js
@@ -1,13 +1,13 @@
 import { onFileUploadChange } from "./fileHandlers.js";
 
-export function handleFileDropzoneOnDrop(event, uploadedFiles, fileType) {
+export async function handleFileDropzoneOnDrop(event, uploadedFiles, fileType) {
     event.preventDefault();
     const dropzoneElement = event.target;
     dropzoneElement.innerHTML = "";
     dropzoneElement.classList.remove("drag-over");
-    onFileUploadChange(event, fileType, uploadedFiles);
+    uploadedFiles = await onFileUploadChange(event, fileType, uploadedFiles);
     dropzoneElement.appendChild(
-        renderUploadedFilesList(event.dataTransfer.files)
+        renderUploadedFilesList(uploadedFiles[fileType])
     );
 }
 
@@ -26,12 +26,38 @@ export function handleFileDropzoneOnDragLeave(event) {
 function renderUploadedFilesList(files) {
     const uploadedFilesListElement = document.createElement("ul");
     uploadedFilesListElement.classList.add("file-list");
-    for (var i = 0; i < files.length; i++) {
-        const file = files[i];
+    Object.keys(files).forEach((fileName) => {
+        const file = files[fileName];
         const fileElement = document.createElement("li");
         fileElement.classList.add("file");
-        fileElement.textContent = file.name;
+        const fileNameElement = document.createElement("span");
+        fileNameElement.textContent = fileName;
+        fileElement.appendChild(fileNameElement);
+        if (file.errors?.length) {
+            fileElement.classList.add("has-errors");
+            const errorListElement = renderUploadedFilesErrorList(file.errors);
+            fileElement.appendChild(errorListElement);
+        }
         uploadedFilesListElement.appendChild(fileElement);
-    }
+    });
     return uploadedFilesListElement;
+}
+
+function renderUploadedFilesErrorList(errors) {
+    const errorListElement = document.createElement("ul");
+    errorListElement.classList.add("error-list");
+    errors.forEach((error) => {
+        const errorElement = document.createElement("li");
+        errorElement.classList.add("error");
+        const errorLabelElement = document.createElement("span");
+        errorLabelElement.classList.add("error-label");
+        errorLabelElement.textContent = `${error.label}: `;
+        const errorMessageElement = document.createElement("span");
+        errorMessageElement.classList.add("error-message");
+        errorMessageElement.textContent = error.message;
+        errorElement.appendChild(errorLabelElement);
+        errorElement.appendChild(errorMessageElement);
+        errorListElement.appendChild(errorElement);
+    });
+    return errorListElement;
 }
--- src/scripts/fileHandlers.js
+++ src/scripts/fileHandlers.js
@@ -1,11 +1,42 @@
-export function onFileUploadChange(event, type, uploadedFiles) {
-    const files = event.dataTransfer.files;
+export async function onFileUploadChange(event, type, uploadedFiles) {
+    const files = event.target.files || event.dataTransfer.files;
+    const promises = [];
     for (var i = 0; i < files.length; i++) {
         const file = files[i];
         const reader = new FileReader();
-        reader.onload = (event) => {
-            uploadedFiles[type][file.name] = JSON.parse(event.target.result);
-        };
-        reader.readAsText(file);
+        const promise = new Promise((resolve, reject) => {
+            reader.onload = (event) => {
+                try {
+                    uploadedFiles[type][file.name] = JSON.parse(
+                        event.target.result
+                    );
+                    resolve();
+                } catch (error) {
+                    if (!uploadedFiles[type][file.name]) {
+                        uploadedFiles[type][file.name] = {
+                            errors: [],
+                        };
+                    }
+                    if (!uploadedFiles[type][file.name].errors) {
+                        uploadedFiles[type][file.name].errors = [];
+                    }
+                    uploadedFiles[type][file.name].errors.push({
+                        label: "Error parsing file",
+                        message: error,
+                    });
+                    reject(error);
+                }
+            };
+            reader.readAsText(file);
+        });
+        promises.push(promise);
     }
+
+    return await Promise.all(promises)
+        .then(() => {
+            return uploadedFiles;
+        })
+        .catch((error) => {
+            return uploadedFiles;
+        });
 }
