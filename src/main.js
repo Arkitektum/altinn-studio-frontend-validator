807dea1e8ac9756c58fe9d391102b7d3106cd9e1
Benjamin Dehli <superelg@gmail.com>
2025-01-28 17:23:28 +0100

    Remove unused functions

--- src/main.js
+++ src/main.js
@@ -8,15 +8,3 @@ const uploadedFiles = {
 window.onload = function () {
     init(uploadedFiles, validationResults);
 };
-
-function dragOverHandler(event) {
-    event.preventDefault();
-    const dropzone = document.querySelector(".dropzone");
-    dropzone.classList.add("dragging_over");
-}
-
-function dragLeaveHandler(event) {
-    event.preventDefault();
-    const dropzone = document.querySelector(".dropzone");
-    dropzone.classList.remove("dragging_over");
-}
